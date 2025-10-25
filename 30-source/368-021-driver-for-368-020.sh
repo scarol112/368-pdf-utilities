db8c3c095103c55c4f5b01e1bff00df5bcf05e2b
Steve Carol <sg.carol@gmail.com>
2025-10-25 14:33:28 -0400

    quoted shell vars

--- 30-source/368-021-driver-for-368-020.sh
+++ 30-source/368-021-driver-for-368-020.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # $Source: /srv/368-pdf-utilities/30-source/RCS/368-021-driver-for-368-020.sh,v $
-# $Date: 2025/10/13 12:39:25 $
-# $Revision: 1.2 $
+# $Date: 2025/10/25 18:32:44 $
+# $Revision: 1.3 $
 # $State: Exp $
 
 # Driver for 368-020, providing the number of pages by inspecting the input file
@@ -11,7 +11,7 @@ echo "Usage: 368-021-driver-for-368-020 INFILE OUTFILE"
 
 INPUT="$1"
 OUTPUT="$2"
-NUMPAGES=$(pdfinfo $INPUT |awk '/Pages:/{print $2}')
+NUMPAGES=$(pdfinfo "$INPUT" |awk '/Pages:/{print $2}')
 
-sh ./368-020-add-page-numbers-to-PDF.sh -i $INPUT -n $NUMPAGES -o $OUTPUT
+sh ./368-020-add-page-numbers-to-PDF.sh -i "$INPUT" -n "$NUMPAGES" -o "$OUTPUT"
 
